0ebb0e02145a6fe4c189c282781ac7528492dfac
Nyan-Steve <nyanstevens@gmail.com>
2022-03-11 18:29:07 -0800

    third commit

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -18,13 +18,15 @@ int main(void)
 			putchar(c);
 			putchar(i);
 
-			if ((c != '9') || (c == '9' && i != '9') '\n')
-			
-			 			
-		       
+			if ((c != '9') || (c == '9' && i != '9'))
+			{
+				putchar(',');
+				putchar(' ');
+			}
 		}
 	}
 
+	putchar('\n');
 
 	return (0);
 }
